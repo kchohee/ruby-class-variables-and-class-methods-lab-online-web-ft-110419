94abf40ac0a5ccdd4744caa1de6898ce9ad7198a
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-14 01:00:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,25 +1,28 @@
 class Song
  attr_accessor :name, :artist, :genre
   @@count = 0
-  @@genres = []
   @@artists = []
+  @@genres = []
+  
   def initialize(name,artist,genre)
     @@name = name
     @@artist = artist
     @@genre = genre
     @@count += 1
-    @@genres << genre
     @@artists << artist
+    @@genres << genre
+    
   end
 def self.count
   @@count
 end
-def self.genres
-  @@genres.uniq
-end
 def self.artists
   @@artists.uniq
 end
+def self.genres
+  @@genres.uniq
+end
+
 def self.genre_counter
   genre_count = Hash.new(0)
     @@genres.each do |genre| genre_count[genre] += 1
