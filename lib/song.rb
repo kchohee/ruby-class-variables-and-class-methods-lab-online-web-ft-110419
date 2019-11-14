0e0b195cfa9960c2a994292c2c8586d7418b28f6
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-14 01:00:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,37 +1,84 @@
+# class Song
+#  attr_accessor :name, :artist, :genre
+#   @@count = 0
+#   @@artists = []
+#   @@genres = []
+# 
+#   def initialize(name,artist,genre)
+#     @@name = name
+#     @@artist = artist
+#     @@genre = genre
+#     @@count += 1
+#     @@artists << artist
+#     @@genres << genre
+#   end
+# def self.count
+#   @@count
+# end
+# def self.artists
+#   @@artists.uniq
+# end
+# def self.genres
+#   @@genres.uniq
+# end
+# 
+# def self.genre_counter
+#   genre_count = Hash.new(0)
+#     @@genres.each do |genre| genre_count[genre] += 1
+#     end
+#     genre_count
+#   end
+# def self.artist_count
+#   artist_count = Hash.new(0)
+#   @@artists.each do |artist| artist_count[artist] += 1
+#   end
+#   artist_count
+#   end
+# end
+
 class Song
- attr_accessor :name, :artist, :genre
+
+  attr_reader :name, :artist, :genre
   @@count = 0
   @@artists = []
   @@genres = []
 
-  def initialize(name,artist,genre)
-    @@name = name
-    @@artist = artist
-    @@genre = genre
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
     @@count += 1
     @@artists << artist
     @@genres << genre
   end
-def self.count
-  @@count
-end
-def self.artists
-  @@artists.uniq
-end
-def self.genres
-  @@genres.uniq
-end
 
-def self.genre_counter
-  genre_count = Hash.new(0)
-    @@genres.each do |genre| genre_count[genre] += 1
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_hash = Hash.new(0)
+    @@genres.each do |genre|
+      genre_hash[genre] += 1
     end
-    genre_count
+    genre_hash
   end
-def self.artist_count
-  artist_count = Hash.new(0)
-  @@artists.each do |artist| artist_count[artist] += 1
+
+  def self.artist_count
+    artist_hash = Hash.new(0)
+    @@artists.each do |artist|
+      artist_hash[artist] += 1
+    end
+    artist_hash
   end
-  artist_count
-  end
-end
+
+end 
